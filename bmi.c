a703a895ff4bd689b9aa14135db1df2918112c36
benjaacuna1407 <benjaacuna590@gmail.com>
2026-07-05 11:24:14 -0300

    bmi.c-actualizado

--- bmi.c
+++ bmi.c
@@ -11,8 +11,20 @@ int main(void) {
 	
 	bmi = peso / (alt * alt);
 	
-	printf ("\n\nSu índice de masa corporal es: %.2f\n\n", bmi);
-	printf ("Indice \t\t|\tCondición\n------------------------------\n<18.5\t\t|\tBajo peso\n18.5 a 24.9\t|\tNormal\n25.0 a 29.9\t|\tSobrepeso\n>=30\t\t|\tObesidad");
+	printf ("\n\nSu Ã­ndice de masa corporal es: %.2f\n\n", bmi);
+	printf ("Indice \t\t|\tCondiciÃ³n\n------------------------------\n<18.5\t\t|\tBajo peso\n18.5 a 24.9\t|\tNormal\n25.0 a 29.9\t|\tSobrepeso\n>=30\t\t|\tObesidad\n\n");
+	
+	if (bmi<18.5)
+		printf ("Usted tiene bajo peso");
+	
+	else if (bmi>=18.5&&bmi<24.9)
+		printf ("Usted tiene peso normal");
+	
+	else if (bmi>=25&&bmi<29.9)
+		printf ("Usted tiene sobrepeso");
+	
+	else 
+		printf ("Usted tiene obesidad");
 	
 	return 0;
 }
